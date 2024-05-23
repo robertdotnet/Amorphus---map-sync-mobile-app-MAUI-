ae75bfd8a95228f89de99eec66efa934d7dc4eb0
Robert Munteanu <rmmnr2@gmail.com>
2024-05-23 13:53:59 +0300

    Add project files.

---
+++ NavPoint/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NavPoint
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
