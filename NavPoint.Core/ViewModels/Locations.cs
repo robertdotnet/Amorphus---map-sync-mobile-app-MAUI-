d4f5ee195e9b27e5bb406b7c57cd9dc5ea521718
Robert Munteanu <rmmnr2@gmail.com>
2024-05-23 14:52:35 +0300

    Added CommuinityToolkit.Mvvm NuGet Locations are now observable

--- NavPoint.Core/ViewModels/Locations.cs
+++ NavPoint.Core/ViewModels/Locations.cs
@@ -1,32 +1,33 @@
-﻿using NavPoint.Core.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NavPoint.Core.Models;
 using NavPoint.Core.Types;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NavPoint.Core.ViewModels;
 
-public class Locations
+public partial class Locations : ObservableObject
 {
-    public List<LocationUnit> LocationUnits {  get; set; }
+    [ObservableProperty]
+    private ObservableCollection<LocationUnit> _locationUnits;
 
     public Locations()
     {
-        LocationUnits = new List<LocationUnit>();
+        _locationUnits = LoadLocations();
     }
 
-    public List<LocationUnit> LoadLocations()
+    private ObservableCollection<LocationUnit> LoadLocations()
     {
-        var locations = new List<LocationUnit>();
+        var locations = new ObservableCollection<LocationUnit>();
 
-        locations.AddRange(new List<LocationUnit>()
-        {
-            new LocationUnit("Home", LocationType.Place, new Coordinates() { XCoordinate = 44.443145, YCoordinate = 26.022156 }),
-            new LocationUnit("Work", LocationType.Place, new Coordinates() { XCoordinate = 44.481202, YCoordinate = 26.115779 }),
-            new LocationUnit("Stefania", LocationType.People, new Coordinates() { XCoordinate = 44.435427, YCoordinate = 26.023330 })
-        });
+        locations.Add(new LocationUnit("Home", LocationType.Place, new Coordinates() { XCoordinate = 44.443145, YCoordinate = 26.022156 }));
+        locations.Add(new LocationUnit("Work", LocationType.Place, new Coordinates() { XCoordinate = 44.481202, YCoordinate = 26.115779 }));
+        locations.Add(new LocationUnit("Stefania", LocationType.People, new Coordinates() { XCoordinate = 44.435427, YCoordinate = 26.023330 }));
+            
         //TODO: logic from source e.g. DB
 
         return locations;
