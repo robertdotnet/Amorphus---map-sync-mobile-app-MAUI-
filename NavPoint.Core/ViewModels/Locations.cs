1a6fecfbd2802be325ae9bb6c44d1d6c2eb848ec
Robert Munteanu <rmmnr2@gmail.com>
2024-07-18 13:23:49 +0300

    refined UI added command for "Navigate" button added Clicked event for "Delete" button

--- NavPoint.Core/ViewModels/Locations.cs
+++ NavPoint.Core/ViewModels/Locations.cs
@@ -8,18 +8,26 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NavPoint.Core.ViewModels;
 
-public partial class Locations : ObservableObject
+[ObservableObject]
+public partial class Locations
 {
+
     [ObservableProperty]
     private ObservableCollection<LocationUnit> _locationUnits;
+
+    int count = 0;
+    [ObservableProperty]
+    private string _testLabel;
 
     public Locations()
     {
         LocationUnits = new ObservableCollection<LocationUnit>();
         LoadCurrentLocations();
+        //NavigateCommand = new RelayCommand(OnClick(LocationUnits.First()), CanExecuteNavigateButton);
     }
 
     [RelayCommand]
@@ -30,7 +38,7 @@ public partial class Locations : ObservableObject
         locations.Add(new LocationUnit("Home", LocationType.Place, new Coordinates() { XCoordinate = 44.443145, YCoordinate = 26.022156 }));
         locations.Add(new LocationUnit("Work", LocationType.Place, new Coordinates() { XCoordinate = 44.481202, YCoordinate = 26.115779 }));
         locations.Add(new LocationUnit("Stefania", LocationType.People, new Coordinates() { XCoordinate = 44.435427, YCoordinate = 26.023330 }));
-            
+
         //TODO: logic from source e.g. DB
 
         LocationUnits = locations;
@@ -41,5 +49,30 @@ public partial class Locations : ObservableObject
     {
         //TODO: validation
         LocationUnits.Add(locationUnit);
+    }
+
+
+    [RelayCommand]
+    public void NavigateCommand() 
+    {
+        new Page().DisplayAlert("test", "You have been alerted", "OK");
+
+        TestLabel = $"Clicked {count}";
+        count ++;
+    }
+    
+    //TODO: maybe validate if Waze is installed?
+    private bool CanExecuteNavigateButton() =>  true;
+
+    private Action OnClick(LocationUnit locationUnit)
+    {
+        return () =>
+        {
+
+            if (locationUnit is LocationUnit unit) // Check if it's a LocationUnit
+            {
+                new Page().DisplayAlert(unit.ToString(), "You have been alerted", "OK");
+            }
+        };
     }
 }
--- NavPoint/MainPage.xaml.cs
+++ NavPoint/MainPage.xaml.cs
@@ -4,12 +4,20 @@ namespace NavPoint;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
+    int count = 1;
     public MainPage()
     {
         InitializeComponent();
-        //BindingContext = locations;
+
+        LocationsCollectionView.ItemsSource = new Locations().LocationUnits;
+        
     }
 
+    private void Button_Delete_Clicked(object sender, EventArgs e)
+    {
+        testLabel.Text = $"clicked {count}";
+        count++;
+
+        DisplayAlert("delete",$"{sender}", "ok");
+    }
 }
